90adc8b83ddc61b7bc45850d34fa210994a6d511
matiasca <matiasca89@hotmail.com>
2014-07-12 20:10:19 -0300

    Set header

---
+++ switchyard-example/src/main/java/com/example/switchyard/example/Header.java
@@ -0,0 +1,10 @@
+package com.example.switchyard.example;
+
+public class Header {
+	
+	public String setHeader(){
+		String header = "switchyard://ServicioB?operationName=apendear,switchyard://ServicioA?operationName=apendear";
+		return header;
+	}
+
+}
