c9d7a816995bed19f5879ef96814b8cfc571da0b
Vanessa Diaz <vdiaz@despegar.com>
2014-07-29 23:19:46 -0300

    A

---
+++ switchyard-example/src/main/java/com/example/switchyard/example/UserLocation.java
@@ -0,0 +1,27 @@
+package com.example.switchyard.example;
+
+public class UserLocation {
+
+	private String ciudad;
+	private String userId;
+	private long timestamp;
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}	
+	
+} 
