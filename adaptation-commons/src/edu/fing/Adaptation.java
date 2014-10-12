d15ba98cf83f507ea15b1fed55e78f2d069812b5
vdiaz <vdiaz@despegar.com>
2014-10-12 17:29:15 -0200

    commons

---
+++ adaptation-commons/src/edu/fing/Adaptation.java
@@ -0,0 +1,22 @@
+package edu.fing;
+
+public class Adaptation {
+
+	private String name;
+	private Object data;
+	
+	public String getName() {
+		return name;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+}
---
+++ adaptation-commons/src/edu/fing/ContextReasonerData.java
@@ -0,0 +1,36 @@
+package edu.fing;
+
+import java.util.List;
+
+public class ContextReasonerData {
+
+	private String user;
+	private String service;
+	private String operation;
+	private List<Adaptation> adaptations;
+	public String getUser() {
+		return user;
+	}
+	public String getService() {
+		return service;
+	}
+	public String getOperation() {
+		return operation;
+	}
+	public List<Adaptation> getAdaptations() {
+		return adaptations;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	public void setAdaptations(List<Adaptation> adaptations) {
+		this.adaptations = adaptations;
+	}
+	
+}
