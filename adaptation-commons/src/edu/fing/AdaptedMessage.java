8b0f6d70f46bcf60f7d63bea577c2ae9536e27dc
matiasca <matiasca89@hotmail.com>
2014-10-21 22:57:13 -0200

    adapted message

---
+++ adaptation-commons/src/edu/fing/AdaptedMessage.java
@@ -0,0 +1,25 @@
+package edu.fing;
+
+import java.util.List;
+
+public class AdaptedMessage {
+
+	private String message;
+	private List<Adaptation> adaptations;
+
+	public List<Adaptation> getAdaptations() {
+		return this.adaptations;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setAdaptations(List<Adaptation> adaptations) {
+		this.adaptations = adaptations;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
