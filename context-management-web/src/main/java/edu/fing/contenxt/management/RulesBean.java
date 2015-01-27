2e2e788dcebcf734963b987c993f172742b5ab36
matiasca <matiasca89@hotmail.com>
2015-01-27 21:33:57 -0200

    Cambios del front

--- context-management-web/src/main/java/edu/fing/contenxt/management/RulesBean.java
+++ context-management-web/src/main/java/edu/fing/contenxt/management/RulesBean.java
@@ -36,6 +36,7 @@ public class RulesBean {
 	public void charge() {
 		this.res = RemoteInvokerUtils.invoke(RemoteInvokerUtils.DroolsConfigService, "getAvailableRules", null, AvailableRulesTO.class);
 		if (this.res == null) {
+
 			this.res = new AvailableRulesTO();
 
 			this.res.setActiveVersionId((long) 3.3);
