4fc220750d95bca8bd13a46eb915be9bcb774414
matiasca <matiasca89@hotmail.com>
2015-02-28 18:18:25 -0200

    Configured DTO

---
+++ adaptation-commons/src/edu/fing/commons/front/dto/ConfiguredItineraryTO.java
@@ -0,0 +1,75 @@
+package edu.fing.commons.front.dto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import edu.fing.commons.dto.AdaptationTO;
+
+public class ConfiguredItineraryTO {
+
+	private String user;
+	private String service;
+	private String operation;
+	private String situation;
+	private int priority;
+	private Date expirationDate;
+	private List<AdaptationTO> adaptationDirective = new ArrayList<AdaptationTO>();
+
+	public List<AdaptationTO> getAdaptationDirective() {
+		return this.adaptationDirective;
+	}
+
+	public Date getExpirationDate() {
+		return this.expirationDate;
+	}
+
+	public String getOperation() {
+		return this.operation;
+	}
+
+	public int getPriority() {
+		return this.priority;
+	}
+
+	public String getService() {
+		return this.service;
+	}
+
+	public String getSituation() {
+		return this.situation;
+	}
+
+	public String getUser() {
+		return this.user;
+	}
+
+	public void setAdaptationDirective(List<AdaptationTO> adaptationDirective) {
+		this.adaptationDirective = adaptationDirective;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+}
