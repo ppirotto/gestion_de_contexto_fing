a1746c4e74eecb95ae8838cdd21327d0a9ba3755
vdiaz <vdiaz@despegar.com>
2015-03-12 20:42:38 -0300

    Sin postconstruct y con http

--- CEP-Engine/src/main/java/edu/fing/cep/engine/bean/DroolsManagerServiceBean.java
+++ CEP-Engine/src/main/java/edu/fing/cep/engine/bean/DroolsManagerServiceBean.java
@@ -34,31 +34,31 @@ public class DroolsManagerServiceBean implements DroolsManagerService {
 	private KieBaseConfiguration streamModeConfig;
 
 	@Inject
-	@Reference	
+	@Reference
 	private CEPService cepService;
-	
-	private static ReentrantReadWriteLock  lock = new ReentrantReadWriteLock();
-	
-	@Override
+
+	private static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+	@PostConstruct
 	public void intializeDroolsContext() {
 		try {
-			//inicializo fields para Drools
-			kServices = KieServices.Factory.get();			
+			// inicializo fields para Drools
+			kServices = KieServices.Factory.get();
 			streamModeConfig = kServices.newKieBaseConfiguration();
-			streamModeConfig.setOption( EventProcessingOption.STREAM );
+			streamModeConfig.setOption(EventProcessingOption.STREAM);
 
-			//Obtengo version activa para deployar
+			// Obtengo version activa para deployar
 			VersionTO activeVersion = cepService.getActiveVersion();
-			
-			//Creo el releaseId
+
+			// Creo el releaseId
 			ReleaseId releaseId1 = kServices.newReleaseId("edu.fing.cep.engine", "drools-context-rules", activeVersion.getVersionNumber());
-			
+
 			List<String> stringRules = getStringRules(activeVersion);
 
-			KieModule kieModule = DroolsUtils.createAndDeployJar( kServices, releaseId1, stringRules);
-		
+			KieModule kieModule = DroolsUtils.createAndDeployJar(kServices, releaseId1, stringRules);
+
 			startSession(kieModule);
-			
+
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
@@ -84,23 +84,23 @@ public class DroolsManagerServiceBean implements DroolsManagerService {
 			lock.writeLock().unlock();
 		}
 	};
-	
+
 	@Override
-	public String insert(ContextualDataTO data){
-		try{
+	public String insert(ContextualDataTO data) {
+		try {
 			lock.readLock().lock();
 			kSession.insert(data);
 			return "OK";
-		} catch(Exception e) {
+		} catch (Exception e) {
 			return "ERROR";
-		} finally{
+		} finally {
 			lock.readLock().unlock();
 		}
 	}
 
 	@Override
 	public CreateRulesVersionResponseTO deployVersion(VersionTO desiredVersion) {
-		
+
 		ReleaseId newReleaseId = kServices.newReleaseId("edu.fing.cep.engine", "drools-context-rules", desiredVersion.getVersionNumber());
 
 		List<String> stringRules = getStringRules(desiredVersion);
@@ -111,41 +111,38 @@ public class DroolsManagerServiceBean implements DroolsManagerService {
 			return buildRulesVersionResponseTO(e);
 		}
 
-		kContainer = kServices.newKieContainer( kieModule.getReleaseId() );
+		kContainer = kServices.newKieContainer(kieModule.getReleaseId());
 		KieBase kBase = kContainer.newKieBase(streamModeConfig);
-		
+
 		startSession(kieModule);
 		return buildRulesVersionResponseTO(null);
 	}
 
-
-
 	/**
-	 * Verifica compilacion de las reglas, retorna null si compilan correctamente
+	 * Verifica compilacion de las reglas, retorna null si compilan
+	 * correctamente
 	 * */
 	@Override
 	public CreateRulesVersionResponseTO testDroolsCompiling(VersionTO versionTO) {
-		try{
-			
-//			VersionTO versionToTest = this.mapToEntity(versionTO);
-			
+		try {
+
+			// VersionTO versionToTest = this.mapToEntity(versionTO);
+
 			ReleaseId newReleaseId = kServices.newReleaseId("edu.fing.cep.engine", "drools-context-rules", versionTO.getVersionNumber());
-	
+
 			List<String> stringRules = getStringRules(versionTO);
 			KieModule kieModule = DroolsUtils.createAndDeployJar(kServices, newReleaseId, stringRules);
-	
-		} catch(DroolsCompilingException e){
+
+		} catch (DroolsCompilingException e) {
 			return buildRulesVersionResponseTO(e);
 		}
 		return null;
-		
+
 	}
-
-
 
 	private CreateRulesVersionResponseTO buildRulesVersionResponseTO(DroolsCompilingException e) {
 		CreateRulesVersionResponseTO res = new CreateRulesVersionResponseTO();
-		if (e==null){
+		if (e == null) {
 			res.setSuccess(true);
 		} else {
 			res.setErrorCode("COMPILING_ERROR");
@@ -154,15 +151,14 @@ public class DroolsManagerServiceBean implements DroolsManagerService {
 		}
 		return res;
 	}
-	
-	
+
 	private List<String> getStringRules(VersionTO desiredVersion) {
-		//Obtengo las reglas asociadas a la version
+		// Obtengo las reglas asociadas a la version
 		List<RuleTO> rules = desiredVersion.getRules();
 		List<String> stringRules = new ArrayList<String>();
-		for(RuleTO rule : rules){
+		for (RuleTO rule : rules) {
 			stringRules.add(rule.getDrl());
 		}
 		return stringRules;
-	}	
+	}
 }
