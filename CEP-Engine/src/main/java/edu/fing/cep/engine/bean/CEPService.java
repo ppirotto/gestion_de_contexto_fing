d9212f96c5ea7b558ff4c33900b9ebebecf7e558
ppirotto <ppirotto@despegar.com>
2015-03-12 19:52:19 -0300

    NO ANDAAAA

---
+++ CEP-Engine/src/main/java/edu/fing/cep/engine/bean/CEPService.java
@@ -0,0 +1,17 @@
+package edu.fing.cep.engine.bean;
+
+import edu.fing.commons.front.dto.AvailableRulesTO;
+import edu.fing.commons.front.dto.CreateRulesVersionResponseTO;
+import edu.fing.commons.front.dto.VersionTO;
+
+public interface CEPService {
+
+	void updateActiveVersion(String versionNumber);
+
+	AvailableRulesTO getAvailableRules();
+
+	CreateRulesVersionResponseTO createNewVersion(VersionTO version);
+
+	VersionTO getActiveVersion();
+
+}
