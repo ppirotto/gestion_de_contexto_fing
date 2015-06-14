cbba9b1dfe17a3cb7f8e5663bbcb1890d55967e8
ppirotto <ppirotto@despegar.com>
2015-06-13 21:05:16 -0300

    borro operaciones que no se usan

--- CEP-Engine/src/main/java/edu/fing/cep/engine/bean/ContextReasonerService.java
+++ CEP-Engine/src/main/java/edu/fing/cep/engine/bean/ContextReasonerService.java
@@ -1,15 +1,8 @@
 package edu.fing.cep.engine.bean;
 
-import edu.fing.commons.front.dto.AvailableRulesTO;
-import edu.fing.commons.front.dto.FrontResponseTO;
 import edu.fing.commons.front.dto.VersionTO;
 
 public interface ContextReasonerService {
-	void updateActiveVersion(String versionNumber);
-
-	AvailableRulesTO getAvailableRules();
-
-	FrontResponseTO createNewVersion(VersionTO version);
-
+	
 	VersionTO getActiveVersion();
 }
